25542e23d8657670933fb09595b5841b973fef2e
munozvicente <vicentemunoz@MBP.fibertel.com.ar>
2022-08-26 23:31:25 -0300

    Partial styling of Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,26 +8,28 @@ const Header = () => {
 
   return (
     <header className="header">
-      <h1 className="MW6">VM.</h1>
-      <nav>
-        <ul>
-          <li className="homeButton">
-            <p>Home</p>
-          </li>
-          <li className="navbarButton">
-            <p>About</p>
-          </li>
-          <li className="navbarButton">
-            <p>Projects</p>
-          </li>
-          <li className="contactButton">
-            <p>CONTACT</p>
-          </li>
-        </ul>
-      </nav>
-      <div>
-        <p className="modeSwitch">ACTIVATE DARK MODE</p>
-        <img src="../img/icons/darkmode.png" alt="" />
+      <div className="wrapper">
+        <h1 className="MW6">VM.</h1>
+        <nav>
+            <ul>
+            <li className="homeButton">
+                <p>Home</p>
+            </li>
+            <li className="navBarButton">
+                <p>About</p>
+            </li>
+            <li className="navBarButton">
+                <p>Projects</p>
+            </li>
+            <li className="contactButton">
+                <p>CONTACT</p>
+            </li>
+            </ul>
+        </nav>
+        <div>
+            <p className="modeSwitch">ACTIVATE DARK MODE</p>
+            <img src="../img/icons/darkmode.png" alt="" />
+        </div>
       </div>
     </header>
   );
