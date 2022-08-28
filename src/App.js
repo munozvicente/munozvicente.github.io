447ce2b5da529bf4843b66e5ba35c8f6b8349c15
munozvicente <vicentemunoz@MBP.fibertel.com.ar>
2022-08-28 13:02:15 -0300

    Manual Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
-// import css
-import React, { useEffect } from "react";
+import React, { useEffect, createContext, useState } from "react";
 import { initGA, trackingPageGA } from "./reactGA";
 
-import Header from './components/Header/Header.js';
-import Hero from './components/Hero/Hero.js';
-import Tools from './components/Tools/Tools.js';
-import Projects from './components/Projects/Projects.js';
-import Contact from './components/Contact/Contact.js';
-import Footer from './components/Footer/Footer.js';
+import "./App.css";
+
+import Header from "./components/Header/Header.js";
+import Hero from "./components/Hero/Hero.js";
+import Tools from "./components/Tools/Tools.js";
+import Projects from "./components/Projects/Projects.js";
+import Contact from "./components/Contact/Contact.js";
+import Footer from "./components/Footer/Footer.js";
+
+export const ThemeContext = createContext(null);
 
 const App = () => {
+
   useEffect(() => {
     initGA();
     trackingPageGA("/home");
   }, []);
 
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
+
   return (
-    <div>
-      <Header />
-      <Hero />
-      <Tools />
-      <Projects />
-      <Contact />
-      <Footer />
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div id={theme}>
+        <Header />
+        <Hero />
+        <Tools />
+        <Projects />
+        <Contact />
+        <Footer />
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
